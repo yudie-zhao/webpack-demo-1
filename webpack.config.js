454fa4927fc73b1b7178fb7473dfa76024b13275
yudie <1641500852@qq.com>
2021-12-16 17:12:56 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,16 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: 'index.[contenthash].js'
     },
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [new HtmlWebpackPlugin({
+        title: 'XDML-写代码啦',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }]
+    },
 }
