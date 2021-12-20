c24d581506e94eee531be1350f230152539b282a
yudie <1641500852@qq.com>
2021-12-20 16:45:51 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import x from './x.js'
 import png from './assets/1.png'
 
-// const div = document.getElementById('app')
-// div.innerHTML = `
+const div = document.getElementById('app')
+div.innerHTML = `
 // <img src="${png}">
 // `
 const button = document.createElement("button")
