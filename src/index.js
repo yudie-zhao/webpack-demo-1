78f5b3618bf9f4a081c97923c032fd0542f15aea
yudie <1641500852@qq.com>
2021-12-20 16:49:42 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import x from './x.js'
 import png from './assets/1.png'
 
 const div = document.getElementById('app')
-div.innerHTML = `
-// <img src="${png}">
-// `
+    // div.innerHTML = `
+    // <img src="${png}">
+    // `
 const button = document.createElement("button")
 div.appendChild(button)
 button.innerText = `懒加载`
